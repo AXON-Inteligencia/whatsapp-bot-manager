604ca843878e9ccd38c84ea5cf97e752edcbf987
AXON-Inteligencia <contapradinheiro44000@gmail.com>
2026-05-10 00:39:08 +0000

    fix: corrigir importação Sidebar em dashboard-layout.tsx

--- components/dashboard/dashboard-layout.tsx
+++ components/dashboard/dashboard-layout.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { DashboardSidebar } from "./sidebar"
+import { Sidebar } from "./sidebar"
 import { DashboardHeader } from "./header"
 
 interface DashboardLayoutProps {
@@ -12,7 +12,7 @@ interface DashboardLayoutProps {
 export function DashboardLayout({ children, title, description }: DashboardLayoutProps) {
   return (
     <div className="flex h-screen overflow-hidden">
-      <DashboardSidebar />
+      <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <DashboardHeader />
         <main className="flex-1 overflow-y-auto p-6">
